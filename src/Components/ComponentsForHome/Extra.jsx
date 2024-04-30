b06bdec4e261af4feeea477daf964fc95ea592cb
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-04-30 16:37:38 +0600

    extra tow section aded

---
+++ src/Components/ComponentsForHome/Extra.jsx
@@ -0,0 +1,10 @@
+
+const Extra = () => {
+    return (
+        <div>
+            <h2 className="text-6xl text-center my-10">Extra Section Two</h2>
+        </div>
+    );
+};
+
+export default Extra;
---
+++ src/Components/ComponentsForHome/Extra_1.jsx
@@ -0,0 +1,11 @@
+
+
+const Extra_1 = () => {
+    return (
+        <div>
+            <h1 className="text-4xl">Extra Section One </h1>
+        </div>
+    );
+};
+
+export default Extra_1;
